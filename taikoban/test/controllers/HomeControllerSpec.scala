f462e62317ace7355cf78b656806373af95e744b
Niisoks <colebrhall@gmail.com>
2024-08-03 00:28:54 +0100

    Example calculator with unit test
    credit https://www.baeldung.com/scala/play-framework-intro

--- taikoban/test/controllers/HomeControllerSpec.scala
+++ taikoban/test/controllers/HomeControllerSpec.scala
@@ -11,35 +11,17 @@ import play.api.test.Helpers._
  *
  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
  */
-class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+class  HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "HomeController GET" should {
 
-    "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
-      val home = controller.index().apply(FakeRequest(GET, "/"))
+    "render a page that prints the sum of two numbers" in {
+      val request = FakeRequest(GET, "/sum/100/200")
+      val sumOfNumbers = route(app, request).get
 
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the application" in {
-      val controller = inject[HomeController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the router" in {
-      val request = FakeRequest(GET, "/")
-      val home = route(app, request).get
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+      status(sumOfNumbers) mustBe OK
+      contentType(sumOfNumbers) mustBe Some("text/html")
+      contentAsString(sumOfNumbers) must include ("The sum is 300!")
     }
   }
 }
